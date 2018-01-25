4e66932735196aea8264e73ecbfe5770b75e85f2
Julio García Pérez <juliojose.garcia@11paths.com>
2018-01-25 20:14:25 +0100

    add cors support

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
 
+var app = express()
 const charactersRoute = require('./routes/characters.route');
 
 // Import DB config
@@ -15,6 +17,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Routes
 app.use('/characters', charactersRoute);
